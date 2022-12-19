f03c518cdf024074f1eb7118bbe4ebc2e8d1dbd4
Meijia <shao.390@osu.edu>
2022-12-19 16:53:46 -0500

    coding update

--- code-and-data/main.m
+++ code-and-data/main.m
@@ -19,21 +19,21 @@ MotifName   = {'Triangle','Vshape'};
 filenamesave1 = './hash_A1';
 Our_method_NetHashing(A1, MotifName,filenamesave1);
 
-%% Do the same with matrix A2 
+%% Do the same with adjacency matrix A2 
 filenamesave2 = './hash_A2';
 Our_method_NetHashing(A2, MotifName,filenamesave2);
 
 
 %% Testing step: to construct network two-sample test based on the hashing information
 %% input
-% filenamesave1: filename of file where store hashing information of A1
-% filenamesave2: filename of file where store hashing information of A2
+% filenamesave1: filename of file where to store hashing information of A1
+% filenamesave2: filename of file where to store hashing information of A2
 % conf_level: alpha
 % cdelta: the parameter determines the variance of artificial noise term \delta_T
 
 %% output
 % p_value: a list P-value corresponding with each Motif
-% conf_int: a matrix and each row represents the Confidence Interval for each Motif
+% conf_int: a matrix with each row representing the Confidence Interval for each Motif
 
 conf_level = 0.05;
 cdelta = 0.01;
