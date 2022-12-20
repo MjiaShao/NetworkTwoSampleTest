2420e7777a2078fad9b85fff3437b8e540f99f21
Meijia <shao.390@osu.edu>
2022-12-19 20:06:37 -0500

    update

--- code-and-data/hashing_storage.m
+++ code-and-data/hashing_storage.m
@@ -14,7 +14,7 @@ indexf = isdir ==0;
 Filesf = Files(indexf);
 for j = 1:length(Filesf)
     filenamesave = strcat("./hash_save/hash_",string(j));
-    A = load(strcat("./data_simulation_100/A_",string(j)));
+    A = load(strcat("./data_simulation_100/",Filesf(j).name));
     A = cell2mat(struct2cell(A));
     Our_method_NetHashing(A, MotifName,filenamesave);
 end
